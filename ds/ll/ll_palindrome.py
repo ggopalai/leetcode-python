d2afb35e29f4bb03fdf65d9c9cd6f9ca09a4aa1b
Gagan Gowda <gagangowda26@gmail.com>
2022-12-18 18:53:46 -0800

    18-12-22

--- ds/ll/ll_palindrome.py
+++ ds/ll/ll_palindrome.py
@@ -21,4 +21,44 @@ class Solution:  #wasn't very optimized. think of another solution. Runtime: 916
             return True 
     
         return False
+
+# constant space solution
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        if not head.next: return True
+        t = head
+        
+        count = 0
+        while t:
+            count += 1
+            t = t.next
+        s = f = head
+
+        while f and f.next:
+            s = s.next
+            f = f.next.next
+        if count % 2 == 1:
+            s = s.next
+        
+        #reverse linked list
+        def reverse(node):
+            p = None
+            c = node
+
+            while c:
+                tmp = c.next
+                c.next = p
+                p = c
+                c = tmp
+            return p
+        sh = reverse(s)
+        
+        t = head 
+        while sh:
+            if t.val != sh.val:
+                return False
+            t = t.next
+            sh = sh.next
+        
+        return True 
         
