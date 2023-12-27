bdb9cf3a40b99611c87724911864ac57f855fbe5
Gagan Gowda <gagangowda26@gmail.com>
2023-12-27 18:42:01 +0530

    minimum time to make rope colorful

---
+++ algo/lc_mediums/minimum_time_to_make_rope_colorful.py
@@ -0,0 +1,21 @@
+class Solution:
+    def minCost(self, colors: str, neededTime: List[int]) -> int:
+        res = 0
+        i = 0
+
+        while i < len(colors) - 1:
+            j = i + 1
+            s = 0
+
+            # need to burst all adjacent balloons except the max
+            while j < len(colors) and colors[j] == colors[i]:
+                j += 1
+
+            # found more than 2 same colored balloons
+            if j - i > 1:
+                res += sum(neededTime[i:j]) - max(neededTime[i:j])
+            
+            i = j
+        
+        return res
+        
